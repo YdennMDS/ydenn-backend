be57a94b03d5c14b8a195277a88a2e192ce06601
Antoinelc7 <antoine.lecorre@yahoo.fr>
2024-07-13 21:24:36 +0200

    added folder config with database connexion system

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 const server = express();
+
+connectDB();
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
