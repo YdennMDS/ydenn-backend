4ac043ca112c6bc14f11e95827b365bb31204884
Antoinelc7 <antoine.lecorre@yahoo.fr>
2024-07-17 22:30:46 +0200

    YD-9 created reset password logic

--- src/api/routes/userRoute.js
+++ src/api/routes/userRoute.js
@@ -4,4 +4,14 @@ module.exports = (server) => {
   server.post("/user/register", userController.userRegister);
   server.post("/user/login", userController.userLogin);
   server.get("/user/confirm/:token", userController.confirmEmail);
+
+  server.post(
+    "/user/reset-password-request",
+    userController.generateResetPasswordToken
+  );
+  server.post(
+    "/user/verify-reset-code",
+    userController.verifyResetPasswordCode
+  );
+  server.post("/user/reset-password", userController.resetPassword);
 };
