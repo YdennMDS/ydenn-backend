c1488797b67a7dec475ca29675a518d2856ab845
Antoinelc7 <antoine.lecorre@yahoo.fr>
2024-11-19 16:52:56 +0100

    YD-17 updated messageController

--- src/api/controllers/messageController.js
+++ src/api/controllers/messageController.js
@@ -6,8 +6,8 @@ exports.createMessage = async (req, res) => {
   try {
     const { message_content, message_room } = req.body;
 
-    // const room = await Room.findById(message_room);
-    const room = "6717a92a6fc6f10596a798ed";
+    const room = await Room.findById(message_room);
+    // const room = "6717a92a6fc6f10596a798ed";
 
     if (!room) {
       return res.status(404).json({ error: "Room introuvable" });
@@ -23,8 +23,8 @@ exports.createMessage = async (req, res) => {
     const newMessage = new Message({
       message_content: cleanMessageContent,
       message_author: req.user.id,
-      // message_room,
-      message_room: "6717a92a6fc6f10596a798ed",
+      message_room,
+      // message_room: "6717a92a6fc6f10596a798ed",
     });
 
     await newMessage.save();
