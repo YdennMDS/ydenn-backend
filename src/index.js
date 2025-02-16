64ac31ed8f20fe5b03734d1c6ae3f6fc3bfeafbc
Antoinelc7 <antoine.lecorre@yahoo.fr>
2025-02-16 22:27:33 +0100

    added socketHandler.js file to handle websocket + updated userModel.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
 const cors = require("cors");
-const express = require("express");
 const connectDB = require("./config/db");
 const http = require("http"); // Import du module HTTP
 const { Server } = require("socket.io"); // Import de Socket.IO
+const express = require("express");
 
 // const hostname = "127.0.0.1";
 const hostname = "0.0.0.0";
@@ -16,7 +16,6 @@ const server = express();
 
 // Créer un serveur HTTP pour permettre à Socket.IO de fonctionner
 const httpServer = http.createServer(server);
-
 // Configurer Socket.IO avec le serveur HTTP
 const io = new Server(httpServer, {
   cors: {
@@ -24,6 +23,9 @@ const io = new Server(httpServer, {
     methods: ["GET", "POST"],
   },
 });
+
+const socketHandler = require("./api/sockets/socketHandler");
+socketHandler(io);
 
 connectDB();
 
@@ -82,35 +84,6 @@ const swaggerOptions = {
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-// Configurer Socket.IO pour gérer les connexions
-io.on("connection", (socket) => {
-  console.log("Un utilisateur s'est connecté :", socket.id);
-
-  // Rejoindre une room
-  socket.on("joinRoom", (roomId) => {
-    socket.join(roomId);
-    console.log(`Utilisateur ${socket.id} a rejoint la room ${roomId}`);
-  });
-
-  // Réception d'un message et diffusion dans la room correspondante
-  socket.on("sendMessage", (data) => {
-    const { roomId, message } = data;
-
-    // Émettre le message à tous les utilisateurs de la room
-    io.to(roomId).emit("receiveMessage", {
-      message,
-      roomId,
-      sentAt: new Date().toISOString(),
-    });
-    console.log(`Message envoyé dans la room ${roomId}:`, message);
-  });
-
-  // Déconnexion
-  socket.on("disconnect", () => {
-    console.log("Un utilisateur s'est déconnecté :", socket.id);
-  });
-});
 
 // // // local server
 // server.listen(port, hostname, () => {
